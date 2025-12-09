0b5197d7e31f7a02d7d47fbc362a509cbc820307
Kyle Liberti <kliberti.us@gmail.com>
2025-12-09 16:38:25 -0500

    Attempt to trigger leader mismatch race condition
    Signed-off-by: Kyle Liberti <kliberti.us@gmail.com>

--- cruise-control/src/test/java/com/linkedin/kafka/cruisecontrol/executor/ExecutorTest.java
+++ cruise-control/src/test/java/com/linkedin/kafka/cruisecontrol/executor/ExecutorTest.java
@@ -871,12 +871,15 @@ public class ExecutorTest extends CCKafkaClientsIntegrationTestHarness {
       }
 
       // Wait for leader movement
-      TopicDescription topicAfterLeaderMove = _cluster.waitForTopicMetadata(tp.topic(), Duration.ofSeconds(60),
-          topicDescription -> topicDescription.partitions().get(tp.partition()).leader().id()
-            == proposal.newLeader().brokerId());
-      assertEquals("The leader should have moved for " + tp,
-                   proposal.newLeader().brokerId().intValue(), topicAfterLeaderMove.partitions().get(tp.partition()).leader().id());
-
+      //TopicDescription topicAfterLeaderMove = _cluster.waitForTopicMetadata(tp.topic(), Duration.ofSeconds(60),
+      //    topicDescription -> topicDescription.partitions().get(tp.partition()).leader().id()
+      //      == proposal.newLeader().brokerId());
+      assertEquals("The leader should have moved for " + tp
+          + " proposal.newReplicas: " + proposal.newReplicas()
+          + " proposal.old.replicas " + proposal.oldReplicas()
+          + " topic.leader " + topic.partitions().get(tp.partition()).leader().id(),
+        proposal.newLeader().brokerId(),
+        Integer.valueOf(topic.partitions().get(tp.partition()).leader().id()));
     }
     if (isTriggeredByUserRequest) {
       EasyMock.verify(mockUserTaskInfo, mockUserTaskManager, mockExecutorNotifier, mockLoadMonitor, mockAnomalyDetectorManager);
